c1ff71453988495cf28188a4c99c8bebc3e3fd16
Mustafa Hepekiz <mustafah@Mustafas-MacBook-Pro-2.local>
2020-08-25 20:25:10 -0500

    Delete function fixed

--- routes/destinations.js
+++ routes/destinations.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const destinationsCtrl = require('../controllers/destinations');
 
 router.post('/flights/:id/destinations', destinationsCtrl.create);
-router.delete('/destinations/:dest/:flight', destinationsCtrl.delete)
+router.post('/destinations/:destid/:flight', destinationsCtrl.delete)
 
 
 module.exports = router;
