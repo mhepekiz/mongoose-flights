eb92a54344aebce051273f037c38ad735ebbe879
Mustafa Hepekiz <mustafah@Mustafas-MacBook-Pro-2.local>
2020-09-10 21:59:16 -0500

    Remote DB

--- config/database.js
+++ config/database.js
@@ -1,15 +1,30 @@
+// const mongoose = require('mongoose');
+
+// mongoose.connect('mongodb://localhost/flights', {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   useCreateIndex: true
+// });
+
+
+// // shortcut to mongoose.connection object
+// const db = mongoose.connection;
+	
+// db.on('connected', function() {
+//   console.log(`Connected to MongoDB at ${db.host}:${db.port}`);
+// });
+
+
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/flights', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true
+mongoose.connect(process.env.DATABASE_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true
 });
 
-
-// shortcut to mongoose.connection object
 const db = mongoose.connection;
-	
+
 db.on('connected', function() {
-  console.log(`Connected to MongoDB at ${db.host}:${db.port}`);
+    console.log(`Connected to MongoDB at ${db.host}:${db.port}`)
 });
